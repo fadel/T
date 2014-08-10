183eda55bc4151f1c0b9de00f7e76b949bd0e507
Samuel Fadel <samuelfadel@gmail.com>
2014-08-10 02:12:55 -0300

    New color theme.

--- src/config.h
+++ src/config.h
@@ -22,8 +22,8 @@
 #define CONFIG_MIN_WIDTH  20
 #define CONFIG_MIN_HEIGHT 5
 
-/* Font name */
-#define CONFIG_FONT_NAME "Terminus 9"
+/* Font */
+#define CONFIG_FONT_NAME "Monospace 9"
 
 /* Color palette definition */
 /* PALETTE_SIZE must be 8, 16, 24 or between 25 and 255, inclusive */
@@ -31,26 +31,26 @@
 
 static const char *CONFIG_COLOR_PALETTE[CONFIG_PALETTE_SIZE] = {
 	"#000000", /* 0 Black */
-	"#a5372e", /* 1 Red */
-	"#4a663c", /* 2 Green */
-	"#7c5618", /* 3 Yellow */
-	"#566060", /* 4 Blue */
-	"#8e4948", /* 5 Magenta */
-	"#6d5b42", /* 6 Cyan */
-	"#8d8d8d", /* 7 White */
-	"#2b2b2b", /* 8 Black */
-	"#e06c5c", /* 9 Red */
-	"#7b996c", /* 10 Green */
-	"#b38849", /* 11 Yellow */
-	"#889393", /* 12 Blue */
-	"#c67c78", /* 13 Magenta */
-	"#a28d73", /* 14 Cyan */
-	"#c6c6c6", /* 15 White */
+	"#95513a", /* 1 Red */
+	"#3a9551", /* 2 Green */
+	"#7e953a", /* 3 Yellow */
+	"#3d558d", /* 4 Blue */
+	"#953a7e", /* 5 Magenta */
+	"#3a7e95", /* 6 Cyan */
+	"#d9d9d9", /* 7 White */
+	"#262626", /* 8 Black */
+	"#c17860", /* 9 Red */
+	"#60c178", /* 10 Green */
+	"#a9c160", /* 11 Yellow */
+	"#5f7aba", /* 12 Blue */
+	"#c160a9", /* 13 Magenta */
+	"#60a9c1", /* 14 Cyan */
+	"#ffffff", /* 15 White */
 };
 
 /* Foreground and background. */
-#define CONFIG_FOREGROUND_COLOR "#757575" /* COLOR_PALETTE[7] */
-#define CONFIG_BACKGROUND_COLOR "#151515" /* COLOR_PALETTE[0] */
+#define CONFIG_FOREGROUND_COLOR "#d9e6f2" /* COLOR_PALETTE[7] */
+#define CONFIG_BACKGROUND_COLOR "#0d1926" /* COLOR_PALETTE[0] */
 
 /* Scrollback lines (0 means disabled; negative means "infinite") */
 #define CONFIG_SCROLLBACK_LINES 2000
